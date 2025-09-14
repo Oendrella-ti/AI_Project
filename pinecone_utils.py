e5c6badf08776cc310ab0aa680aef61ea29c50a4
Oendrella <anacatlove97@gmail.com>
2025-09-14 11:38:12 +0530

    pinecone_utils.py

--- pinecone_utils.py
+++ pinecone_utils.py
@@ -6,16 +6,17 @@ embed_model = SentenceTransformer('all-MiniLM-L6-v2')
 
 # Pinecone Init
 pc = Pinecone(api_key="YOUR_PINECONE_API_KEY")
-index = pc.Index("chatbot-index")
+index = pc.Index("my-chat-index")
 #pinecone.init(api_key="YOUR_PINECONE_API_KEY", environment="us-east-1")
-#index_name = "chatbot-index"
+index_name = "my-chat-index"
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 # Create index if it doesn't exist
-if index_name not in pinecone.list_indexes():
-    pinecone.create_index(index_name, dimension=384)
+#if index_name not in pinecone.list_indexes():
+if index_name not in pc.list_indexes().names()
+    pc.create_index(name=index_name, dimension=384, metric="cosine")
 
-index = pinecone.Index(index_name)
+index = pc.Index(index_name)
 
 #def store_query(query, metadata):
  #   vector = embed_model.encode([query])[0]
